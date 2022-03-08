6ba06b1accebf8fc5066acfffa38ca0c9c46a065
gamaliel <gamaliel.aq96@gmail.com>
2022-03-08 14:58:15 -0500

    works

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,3 +1,4 @@
+import { TipsComponent } from './tips/tips.component';
 import { ProfileComponent } from './../components/profile/profile.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -8,6 +9,10 @@ const routes: Routes = [
         path: '',
         component: PagesComponent,
         loadChildren: () => import('./child.routing').then((m) => m.ChildRoutsModule)
+    },
+    {
+        path: 'tips',
+        component: TipsComponent,
     },
     {
         path: 'not-found',
---
+++ src/app/pages/tips/tips.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TipsComponent } from './tips.component';
+
+
+
+@NgModule({
+  declarations: [TipsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TipsModule { }
